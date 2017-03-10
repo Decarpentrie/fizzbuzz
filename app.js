53f5958d6e1af9fda6ec153d49023aeb4a47b420
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-03-10 10:56:06 +0100

    rajout var fizzbuzz, for, if

---
+++ app.js
@@ -0,0 +1,14 @@
+var fizzbuzz = Math.floor(Math.random() * 50);
+
+for (var fizzbuzz = 0; fizzbuzz <=50 ; fizzbuzz ++ ) {
+	
+	if (fizzbuzz % 3 == 0 ){
+		console.log("fizz");
+} else if (fizzbuzz % 5 == 0 ){
+	console.log("buzz");
+}else (fizzbuzz % 15 == 0)
+	console.log("fizzbuzz");
+}
+
+
+
